709163da549992816d63ad9509670aa86918b66c
thibaut.roekens <troekens@gmail.com>
2019-02-18 12:05:12 +0100

    Toggle menu fixed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -66,15 +66,12 @@ class Navbar extends Component {
                     <Link className="navbar-brand" to="/">
                         BlockDapps
                     </Link>
-                    <button
-                        className="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded"
-                        type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         Menu
-                        <i className="fas fa-bars"></i>
+                        <i class="fas fa-bars"></i>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="mobile-nav">
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
                                 <Link className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
